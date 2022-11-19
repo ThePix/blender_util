c7921575a006745d9b3bbac137675eccbfafcc2b
ThePix <the_pix@hotmail.com>
2022-11-19 13:00:38 +0000

    Update bmesh_vary.py
    So now it actually works!

--- bmesh_vary.py
+++ bmesh_vary.py
@@ -1,7 +1,12 @@
-# This example assumes we have a mesh object selected
+# This assumes we have a mesh object selected
 
 import bpy
 import bmesh
+import random
+
+# How much variation do you want? One seems a good starting point, 5 is to much for a brick
+degree = 5
+
 
 # Get the active mesh
 me = bpy.context.object.data
@@ -11,10 +16,18 @@ me = bpy.context.object.data
 bm = bmesh.new()   # create an empty BMesh
 bm.from_mesh(me)   # fill it in from a Mesh
 
+random.seed()
+
+
+def vary():
+    return random.randint(-50, 50) / 500 * degree
+
 
 # Modify the BMesh, can do anything here...
 for v in bm.verts:
-    v.co.x += 1.0
+    v.co.x += vary()
+    v.co.y += vary()
+    v.co.z += vary()
 
 
 # Finish up, write the bmesh back to the mesh
